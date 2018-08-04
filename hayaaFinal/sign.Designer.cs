8197df878df147764adf7cd732a4da52f88f3b07
ali remaity <aliremaitybook@gmail.com>
2018-08-05 02:38:15 +0300

    add sign in form and delete other files

---
+++ hayaaFinal/sign.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace hayaaFinal
+{
+    partial class sign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.usernametext = new System.Windows.Forms.TextBox();
+            this.passwordtext = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2016 Black";
+            // 
+            // usernametext
+            // 
+            this.usernametext.Location = new System.Drawing.Point(281, 35);
+            this.usernametext.Name = "usernametext";
+            this.usernametext.Size = new System.Drawing.Size(128, 20);
+            this.usernametext.TabIndex = 0;
+            // 
+            // passwordtext
+            // 
+            this.passwordtext.Location = new System.Drawing.Point(281, 94);
+            this.passwordtext.Name = "passwordtext";
+            this.passwordtext.Size = new System.Drawing.Size(128, 20);
+            this.passwordtext.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::hayaaFinal.Properties.Resources.passcrack;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 186);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.passwordtext);
+            this.Controls.Add(this.usernametext);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(449, 225);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(449, 225);
+            this.Name = "sign";
+            this.Opacity = 0.9D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "sign";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private System.Windows.Forms.TextBox usernametext;
+        private System.Windows.Forms.TextBox passwordtext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
